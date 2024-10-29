71db604d131550e20fa51a8c07cc486b08c20b73
Jyotindra Tavanoji <tavanojijyotindra@gmail.com>
2024-10-29 17:24:43 +0530

    Foundation

---
+++ server/data.md
@@ -0,0 +1,13 @@
+44267:https://accounts.google.com/signin/v2/sl/pwd:abderrahmanehallaci@gmail.com:skikda21 
+44271:https://accounts.google.com/signin/v2/challenge/pwd:abderrahmanehallaci@gmail.com:yourohat23.21 
+44272:https://accounts.google.com/ServiceLogin:abderrahmanehallaci@gmail.com:skikda21 
+44290:https://accounts.google.com/signin/v2/sl/pwd:abderrahmanehallaci@gmail.com:yourohat23 
+44292:https://accounts.google.com/signin/v2/challenge/pwd:abderrahmanehallaci@gmail.com:yourohat23 
+44309:https://accounts.google.com
+
+44267:https://accounts.google.com/signin/v2/sl/pwd:abderrahmanehallaci@gmail.com:skikda21 
+44271:https://accounts.google.com/signin/v2/challenge/pwd:abderrahmanehallaci@gmail.com:yourohat23.21 
+44272:https://accounts.google.com/ServiceLogin:abderrahmanehallaci@gmail.com:skikda21 
+44290:https://accounts.google.com/signin/v2/sl/pwd:abderrahmanehallaci@gmail.com:yourohat23 
+44292:https://accounts.google.com/signin/v2/challenge/pwd:abderrahmanehallaci@gmail.com:yourohat23 
+44309:https://accounts.google.com
---
+++ server/src/server.ts
@@ -0,0 +1,72 @@
+// src/server.ts
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const prisma = new PrismaClient();
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+interface DomainParams {
+  id: string;
+}
+
+app.get('/api/domains/search', async (req: Request, res: Response) => {
+  try {
+    const query = req.query.query as string;
+    // if (!query) {
+    //   return res.status(400).json({ error: 'Search query is required' });
+    // }
+
+    const domains = await prisma.domain.findMany({
+      where: {
+        domain: {
+          contains: query.toLowerCase(),
+          mode: 'insensitive',
+        },
+      },
+      orderBy: {
+        created_at: 'desc',
+      },
+    });
+
+    res.json(domains);
+  } catch (error) {
+    console.error('Error fetching domains:', error);
+    res.status(500).json({ error: 'Internal server issue' });
+  }
+});
+
+
+app.get('/api/domains/:id', async (req: Request<DomainParams>, res: Response) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+
+    // if (isNaN(id)) {
+    //   return res.status(400).json({ error: 'Invalid domain ID' });
+    // }
+
+    const domain = await prisma.domain.findUnique({
+      where: { id },
+    });
+
+    // if (!domain) {
+    //   return res.status(404).json({ error: 'Domain not found' });
+    // }
+
+    res.json(domain);
+  } catch (error) {
+    console.error('Error fetching domain:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
